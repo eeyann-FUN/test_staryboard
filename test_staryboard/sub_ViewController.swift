3761826538ef92da9e85fb16ac9359ad6c9d6bfc
yazuyazuya <kzyono-1999@docomo.ne.jp>
2018-12-21 18:15:07 +0900

    aaaa

---
+++ test_staryboard/sub_ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  sub_ViewController.swift
+//  test_staryboard
+//
+//  Created by 大野和也 on 2018/12/21.
+//  Copyright © 2018 Tatsuika. All rights reserved.
+//
+
+import UIKit
+
+class sub_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
